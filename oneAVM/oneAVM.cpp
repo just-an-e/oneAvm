2e080762899d9c4a99592f3a274eb237f13401bb
ETTC <ETTC@107-17>
2020-02-25 10:44:29 -0500

    e

---
+++ oneAVM/oneAVM.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector <double> finalGrade{85.8, 93.7, 76, 88.5, 100, 91.3};
+
+	for (int i = 0; i < finalGrade.max_size; i++) {
+
+	}
+
+}
